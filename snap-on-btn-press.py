ab748a5ae4fac9f9ad76fc9f3e79062ff13f3b43
Natan Facchin <maxmousee@gmail.com>
2020-11-14 19:58:08 +0100

    Create simple interface for taking pictures

--- snap-on-btn-press.py
+++ snap-on-btn-press.py
@@ -1,41 +1,42 @@
+import threading
 import time
-import RPi.GPIO as GPIO
-from picamera import PiCamera
-from time import sleep
+import tkinter as tk
 
 
-def setup_gpio():
-    GPIO.setwarnings(False)  # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-    GPIO.setup(10, GPIO.IN,
-               pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+def capture_after_10_secs():
+    print("Will take a picture after 10 seconds")
+    start_time = threading.Timer(10, capture)
+    start_time.start()
 
 
-def setup_camera():
-    camera.brightness = 50
-    camera.sharpness = 0
-    camera.contrast = 0
-    camera.saturation = 0
-    camera.iso = 0
-    camera.exposure_compensation = 0
-    camera.exposure_mode = 'auto'
-    camera.meter_mode = 'average'
-    camera.awb_mode = 'auto'
-    camera.rotation = 0
-    camera.hflip = False
-    camera.vflip = False
-    camera.crop = (0.0, 0.0, 1.0, 1.0)
-    camera.resolution = (4056, 3040)
-    # you may need to increase gpu_mem in /boot/config.txt to achieve full resolution with the Camera Module v2.
+def capture_after_15_secs():
+    print("Will take a picture after 15 seconds")
+    start_time = threading.Timer(15, capture)
+    start_time.start()
+
+
+def capture():
+    print("Taking picture...")
+    filename = "/home/pi/Pictures/image_" + str(int(time.time())) + ".jpg"
+    print("Saved image as " + filename)
 
 
 if __name__ == '__main__':
-    setup_gpio()
-    setup_camera()
+    root = tk.Tk()
+    frame = tk.Frame(root)
+    frame.pack()
+    capture_btn = tk.Button(frame, text="Capture", height=10, width=30, command=capture)
+    capture_btn.pack(side=tk.TOP)
 
-    while True:
-        if GPIO.input(10) == GPIO.HIGH:
-            print("Shutter button was pressed, saving photo...")
-            filename = "image_" + str(int(time.time())) + ".jpg"
-            camera.capture("/home/pi/Pictures/" + filename)
-            sleep(1)
+    capture_after_10_secs_btn = tk.Button(frame, text="Capture after 10 secs", height=10, width=30,
+                                          command=capture_after_10_secs)
+    capture_after_10_secs_btn.pack(side=tk.TOP)
+
+    capture_after_15_secs_btn = tk.Button(frame, text="Capture after 15 secs", height=10, width=30,
+                                          command=capture_after_15_secs)
+    capture_after_15_secs_btn.pack(side=tk.TOP)
+
+    quit_btn = tk.Button(frame, text="QUIT", fg="red", height=10, width=30, command=quit)
+    quit_btn.pack(side=tk.BOTTOM)
+
+    root.mainloop()
